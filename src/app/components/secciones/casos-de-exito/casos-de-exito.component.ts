b68e112fa52c97581502ea1e249b1470df81cd8e
RiveroAA <a.rivero.a250@gmail.com>
2023-09-15 12:26:36 -0300

    Actualizar rama agus

---
+++ src/app/components/secciones/casos-de-exito/casos-de-exito.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { TarjetasService } from 'src/app/services/tarjetas/tarjetas.service';
+
+interface Tarjetas {
+  attributes: {
+    nombre: string
+    tarjetas: {
+      data: {
+        attributes: {
+          titulo: string | null,
+          subtitulo: string | null,
+          texto: string | null,
+          boton: string | null,
+          imagenes: {
+            data: {
+              attributes: {
+                url: string;
+              };
+            }[];
+          };
+        };
+      }[];
+    };
+  };
+}
+
+@Component({
+  selector: 'app-casos-de-exito',
+  templateUrl: './casos-de-exito.component.html',
+  styleUrls: ['./casos-de-exito.component.css']
+})
+export class CasosDeExitoComponent {
+  title = 'Mobeats';
+  tarjetas: Tarjetas[] = [];
+  isFirstIteration: boolean = true;
+
+  constructor(private tarjetasService: TarjetasService) { }
+
+  ngOnInit() {
+    this.mostrarTarjetasCasosDeExito();
+  }
+
+  mostrarTarjetasCasosDeExito() {
+    this.tarjetasService.pedirTarjetasCasosDeExito().subscribe((response: any) => {
+      this.tarjetas = response.data;
+    });
+  }
+}
