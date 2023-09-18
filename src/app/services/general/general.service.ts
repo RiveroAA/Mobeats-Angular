9ce99ece43a265ac4441279d4999506df915b793
nahudev05 <nahueldev05@gmail.com>
2023-09-18 09:58:54 -0300

    Update general.service.ts

--- src/app/services/general/general.service.ts
+++ src/app/services/general/general.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeneralService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  pedirSucursales() {
+    return this.http.get<any[]>('http://localhost:1337/api/sucursals?populate=*');
+  }
+  pedirRedes() {
+    return this.http.get<any[]>('http://localhost:1337/api/redes?populate=*');
+  }
+  pedirTecnologias() {
+    return this.http.get<any[]>('http://localhost:1337/api/tecnologias?populate=*');
+  }
+  pedirEmpleados() {
+    return this.http.get<any[]>('http://localhost:1337/api/empleados?populate=*');
+  }
+  pedirServicios() {
+    return this.http.get<any[]>('http://localhost:1337/api/servicios?populate=*');
+  }
 }
